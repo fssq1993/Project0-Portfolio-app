800c1d4f382904786fd07cfe72a090190a502803
Yuanyi Yang <fssq1993@outlook.com>
2016-12-28 09:53:29 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/android/mynanodegreeapps/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.mynanodegreeapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void spotify(View view){
+
+    }
+    public void scores(View view){
+
+    }
+    public void library(View view){
+
+    }
+    public void build(View view){
+
+    }
+    public void xyz(View view){
+
+    }
+    public void capstone(View view){
+
+    }
+}
